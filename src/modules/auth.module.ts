81b38cf8c1c5bb6aad9a43b59f920df41f673c33
vlad_vitkovskyi <vvitkovskij4@gmail.com>
2024-10-16 21:45:06 +0300

    chore: relocate modules

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './controllers/auth/auth.controller';
-import { AuthService } from './services/auth.service';
+import { AuthController } from '../controllers/auth/auth.controller';
+import { AuthService } from '../services/auth.service';
 
 @Module({
   imports: [],
